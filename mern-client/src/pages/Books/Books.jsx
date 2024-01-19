36bd9f0fc91542e012cc276323237362d319b596
vaishnavirbhat <112920991+vaishnavirbhat26@users.noreply.github.com>
2024-01-19 23:38:45 +0530

    Update Books.jsx

--- mern-client/src/pages/Books/Books.jsx
+++ mern-client/src/pages/Books/Books.jsx
@@ -5,7 +5,7 @@ const Books = () => {
   const [books, setBooks] = useState([]);
 
   useEffect( () => {
-      fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => setBooks(data));
+      fetch("https://book-app-api-lime.vercel.app/all-books").then(res => res.json()).then(data => setBooks(data));
   },[])
 
   return (
