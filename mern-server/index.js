b6413fe3baac7563618c67185cce1a20f602e039
vaishnavirbhat <112920991+vaishnavirbhat26@users.noreply.github.com>
2024-01-19 23:12:44 +0530

    Update index.js

--- mern-server/index.js
+++ mern-server/index.js
@@ -4,7 +4,13 @@ const port = process.env.PORT || 5000
 const cors = require('cors')
 
 //middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: [],
+        methods: ["POST", "GET", "PATCH"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 
